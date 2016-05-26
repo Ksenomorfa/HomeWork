4cde0e2fcc08d224e91c32a0c0c4607fb0c47f96
p.bevad.spb <pbevad@sang.ru>
2016-05-26 10:49:35 +0300

    HomeWork Quadration v3

--- src/main/java/HomeWork1Lesson/Quadration.java
+++ src/main/java/HomeWork1Lesson/Quadration.java
@@ -4,6 +4,8 @@ package HomeWork1Lesson;
  * Not Default
  */
 
+import com.sun.org.apache.xml.internal.serializer.utils.SystemIDResolver;
+
 import java.util.Scanner;
 
 public class Quadration {
@@ -35,32 +37,40 @@ public class Quadration {
         }
         else System.out.println("Некорректное a");
     }
-    //сделать отдельно методы calcD, calcX1, calcX2  и их тесты
 
     public static String calc(double a, double b, double c) {
 
         double D;
         if ((a == 0) && (b == 0) && (c == 0)) {
+            System.out.println("Все a,b,c равны 0");
             return "x - любое число";
         }
         D = b * b - 4 * a * c;
+        System.out.println ("Квадрат дискриминанта = b * b - 4 * a * c  и равен " + D);
         if (D == 0) {
             if (c==0) {
                 return "x равен " + String.format("%.4f", (-b / (2 * a)));
             }
-            else return "Корней нет";
+            else {
+                System.out.println("a и b равны 0");
+                return "Корней нет";
+            }
         }
         if (D > 0) {
             double x1;
             double x2;
             if (a == 0) {
                 x1 = (-c / b);
+                System.out.println("x1 = (-c / b)");
                 return "Уравнение не квадратное, x равен " + String.format("%.4f", Math.abs(x1));
             }
             x1 = ((-b + Math.sqrt(D)) / (2 * a));
+            System.out.println("x1 = ((-b + корень(D)) / (2 * a))");
             x2 = ((-b - Math.sqrt(D)) / (2 * a));
+            System.out.println("x2 = ((-b - корень(D)) / (2 * a))");
             return "x1 равен " + String.format("%.4f", x1) + " x2 равен " + String.format("%.4f", x2);
         }
+
         return "D меньше 0, корней нет";
     }
 }
