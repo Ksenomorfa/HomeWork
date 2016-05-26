d1c052fba7cd60c8fd75204da03380055a4a448c
p.bevad.spb <pbevad@sang.ru>
2016-05-26 10:36:26 +0300

    HomeWork Quadration

--- src/main/java/HomeWork1Lesson/Quadration.java
+++ src/main/java/HomeWork1Lesson/Quadration.java
@@ -3,6 +3,62 @@ package HomeWork1Lesson;
 /**
  * Not Default
  */
+
+import java.util.Scanner;
+
 public class Quadration {
 
+    public static void main(String[] args) {
+
+
+       Scanner s = new Scanner(System.in);
+        System.out.println("Введите a:");
+        if (s.hasNextDouble()) {
+            double a = s.nextDouble();
+
+            System.out.println("Введите b:");
+            if (s.hasNextDouble()) {
+                double b = s.nextDouble();
+
+                System.out.println("Введите c:");
+                if (s.hasNextDouble()) {
+                    double c = s.nextDouble();
+
+                    Quadration calcX = new Quadration();
+                    String s2 = calcX.calc(a, b, c);
+                    System.out.println("Результат: " + s2);
+                }
+                else System.out.println("Некорректное c");
+
+            }
+            else System.out.println("Некорректное b");
+        }
+        else System.out.println("Некорректное a");
+    }
+    //сделать отдельно методы calcD, calcX1, calcX2  и их тесты
+
+    public static String calc(double a, double b, double c) {
+
+        double D;
+        if ((a == 0) && (b == 0) && (c == 0)) {
+            return "x - любое число";
+        }
+        D = b * b - 4 * a * c;
+        if (D == 0) {
+
+            return "x равен " + String.format("%.4f", (-b / (2 * a)));
+        }
+        if (D > 0) {
+            double x1;
+            double x2;
+            if (a == 0) {
+                x1 = (-c / b);
+                return "Уравнение не квадратное, x равен " + String.format("%.4f", Math.abs(x1));
+            }
+            x1 = ((-b + Math.sqrt(D)) / (2 * a));
+            x2 = ((-b - Math.sqrt(D)) / (2 * a));
+            return "x1 равен " + String.format("%.4f", x1) + " x2 равен " + String.format("%.4f", x2);
+        }
+        return "D меньше 0, корней нет";
+    }
 }
--- src/test/java/HomeWork1Lesson/QuadrationTest.java
+++ src/test/java/HomeWork1Lesson/QuadrationTest.java
@@ -1,9 +1,28 @@
 package HomeWork1Lesson;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Not Default
  */
-public class QuadrationTest {
-    int i;
+public class QuadrationTest extends Assert {
+    @Test
+    public void test() {
+
+        assertEquals("D меньше 0, корней нет", Quadration.calc(2,4,7));
+        assertEquals("x равен 2,0000", Quadration.calc (1,-4,4));
+        assertEquals("x1 равен 0,5000 x2 равен -3,0000", Quadration.calc(2,5,-3));
+
+        assertEquals("x1 равен -0,2500 x2 равен -5,0000", Quadration.calc(4,21,5));
+
+        assertEquals("x1 равен 3,0000 x2 равен 0,3333", Quadration.calc(3,-10,3));
+
+
+        assertEquals("x - любое число", Quadration.calc(0, 0, 0));
+        assertEquals("Уравнение не квадратное, x равен 1,0000", Quadration.calc(0, 2, -2));
+        assertEquals("Уравнение не квадратное, x равен 0,0000", Quadration.calc(0, 2, 0));
+
+    }
 
 }
